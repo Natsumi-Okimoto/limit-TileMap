e5101772cd59132639c3cf45a673cb6bdd60acc7
Natsumi-Okimoto <legendpocky.7230@gmail.com>
2021-07-17 16:52:58 +0900

    part7

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,10 +12,14 @@ public class PlayerController : MonoBehaviour
 
     private float horizontal;
     private float vertical;
+
+    private Animator anim;
+    private Vector2 lookDirection = new Vector2(0, -1.0f);
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,6 +27,8 @@ public class PlayerController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        SyncMoveAnimation();
     }
 
     private void FixedUpdate()
@@ -35,5 +41,21 @@ public class PlayerController : MonoBehaviour
     private void Move()
     {
         rb.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+    }
+
+    private void SyncMoveAnimation()
+    {
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        anim.SetFloat("Look X", lookDirection.x);
+        anim.SetFloat("Look Y", lookDirection.y);
+
+        anim.SetFloat("Speed", move.magnitude);
     }
 }
